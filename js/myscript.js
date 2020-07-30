24b1994134c8641117bcb1411e73ff3acb22b1f3
vale <valemil.vs@gmail.com>
2020-07-30 15:30:06 +0200

    push 1

---
+++ js/myscript.js
@@ -0,0 +1,17 @@
+// Milestone 2
+// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e, cliccando invia (l'icona o anche con enter), il testo viene aggiunto al thread sopra, come messaggio verde.
+// Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+// Suggerimento: Utilizzate il template come vi ho fatto vedere a lezione, ma soprattutto state attenti a trovare l'elemento giusto dove inserire (appendere) i valori ricavati dal campo input.
+// In poche parole se il mio template prevede un tag p (possibilmente con una determinata classe) dove inserire del testo, devo trovarlo e poi aggiungere il testo!
+// Ricordatevi anche che alla fine il clone va pushato (incollato) dove desiderate.
+
+// //faccio il clone del template
+// var elemento = $('.template .messaggio').clone();
+// //personalizzo il clone aggiungendo del testo dove voglio
+// elemento.find('.titolo').append('ciao sono il titolo')
+// elemento.find('.descrizione').append('ciao sono lo span');
+// //aggiungo la classe
+// elemento.addClass('rosso');
+//
+// //inserisco l'elemento clonato nel DOM
+// $('.elementi-copiati').append(elemento);
